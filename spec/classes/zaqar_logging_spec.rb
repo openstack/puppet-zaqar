00bf15fc2ec8ed20659fbda7a0d06b0fa212e955
Mykyta Karpin <mkarpin@mirantis.com>
2017-04-05 11:08:00 +0300

    oslo log: check puppet resource instead of actual config in spec
    Change-Id: Idf9cb581c5c8831d15a97a62d9a47efbb937d373

--- spec/classes/zaqar_logging_spec.rb
+++ spec/classes/zaqar_logging_spec.rb
@@ -41,40 +41,25 @@ describe 'zaqar::logging' do
 
   shared_examples_for 'logging params set' do
     it 'enables logging params' do
-      is_expected.to contain_zaqar_config('DEFAULT/logging_context_format_string').with_value(
-        '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [%(request_id)s %(user_identity)s] %(instance)s%(message)s')
-
-      is_expected.to contain_zaqar_config('DEFAULT/logging_default_format_string').with_value(
-        '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [-] %(instance)s%(message)s')
-
-      is_expected.to contain_zaqar_config('DEFAULT/logging_debug_format_suffix').with_value(
-        '%(funcName)s %(pathname)s:%(lineno)d')
-
-      is_expected.to contain_zaqar_config('DEFAULT/logging_exception_prefix').with_value(
-       '%(asctime)s.%(msecs)03d %(process)d TRACE %(name)s %(instance)s')
-
-      is_expected.to contain_zaqar_config('DEFAULT/log_config_append').with_value(
-        '/etc/zaqar/logging.conf')
-      is_expected.to contain_zaqar_config('DEFAULT/publish_errors').with_value(
-        true)
-
-      is_expected.to contain_zaqar_config('DEFAULT/default_log_levels').with_value(
-        'amqp=WARN,amqplib=WARN,boto=WARN,iso8601=WARN,requests.packages.urllib3.connectionpool=WARN,sqlalchemy=WARN,suds=INFO')
-
-      is_expected.to contain_zaqar_config('DEFAULT/fatal_deprecations').with_value(
-        true)
-
-      is_expected.to contain_zaqar_config('DEFAULT/instance_format').with_value(
-        '[instance: %(uuid)s] ')
-
-      is_expected.to contain_zaqar_config('DEFAULT/instance_uuid_format').with_value(
-        '[instance: %(uuid)s] ')
-
-      is_expected.to contain_zaqar_config('DEFAULT/log_date_format').with_value(
-        '%Y-%m-%d %H:%M:%S')
+      is_expected.to contain_oslo__log('zaqar_config').with(
+        :logging_context_format_string =>
+          '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [%(request_id)s %(user_identity)s] %(instance)s%(message)s',
+        :logging_default_format_string => '%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [-] %(instance)s%(message)s',
+        :logging_debug_format_suffix   => '%(funcName)s %(pathname)s:%(lineno)d',
+        :logging_exception_prefix      => '%(asctime)s.%(msecs)03d %(process)d TRACE %(name)s %(instance)s',
+        :log_config_append             => '/etc/zaqar/logging.conf',
+        :publish_errors                => true,
+        :default_log_levels            => {
+          'amqp' => 'WARN', 'amqplib' => 'WARN', 'boto' => 'WARN',
+          'sqlalchemy' => 'WARN', 'suds' => 'INFO', 'iso8601' => 'WARN',
+          'requests.packages.urllib3.connectionpool' => 'WARN' },
+       :fatal_deprecations             => true,
+       :instance_format                => '[instance: %(uuid)s] ',
+       :instance_uuid_format           => '[instance: %(uuid)s] ',
+       :log_date_format                => '%Y-%m-%d %H:%M:%S',
+      )
     end
   end
-
 
   shared_examples_for 'logging params unset' do
    [ :logging_context_format_string, :logging_default_format_string,
@@ -83,7 +68,7 @@ describe 'zaqar::logging' do
      :default_log_levels, :fatal_deprecations,
      :instance_format, :instance_uuid_format,
      :log_date_format, ].each { |param|
-        it { is_expected.to contain_zaqar_config("DEFAULT/#{param}").with(:value => '<SERVICE DEFAULT>') }
+        it { is_expected.to contain_oslo__log('zaqar_config').with("#{param}" => '<SERVICE DEFAULT>') }
       }
   end
 

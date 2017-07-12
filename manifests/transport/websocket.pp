610d9bc987ac1639d76fbc8bd940285777c110ee
Thomas Herve <therve@redhat.com>
2017-07-12 14:56:44 +0200

    Expose websocket notification settings
    This exposes new settings for the websocket service.
    
    Change-Id: I75d5708bec88d9834f2cb4cbad414233854520ad

--- manifests/transport/websocket.pp
+++ manifests/transport/websocket.pp
@@ -12,10 +12,20 @@
 #   Port on which the service is provided to the user.
 #   Defaults to $::os_service_default.
 #
+# [*notification_bind*]
+#   Address on which the notification server will listen.
+#   Defaults to $::os_service_default.
+#
+# [*notification_port*]
+#   Port on which the notification server will listen.
+#   Defaults to $::os_service_default.
+#
 class zaqar::transport::websocket(
-  $bind           = $::os_service_default,
-  $port           = $::os_service_default,
-  $external_port  = $::os_service_default,
+  $bind               = $::os_service_default,
+  $port               = $::os_service_default,
+  $external_port      = $::os_service_default,
+  $notification_bind  = $::os_service_default,
+  $notification_port  = $::os_service_default,
 ) {
 
   include ::zaqar::deps
@@ -24,6 +34,8 @@ class zaqar::transport::websocket(
     'drivers:transport:websocket/bind': value => $bind;
     'drivers:transport:websocket/port': value => $port;
     'drivers:transport:websocket/external-port': value => $external_port;
+    'drivers:transport:websocket/notification-bind': value => $notification_bind;
+    'drivers:transport:websocket/notification-port': value => $notification_port;
   }
 
 }
--- spec/classes/zaqar_transport_websocket_spec.rb
+++ spec/classes/zaqar_transport_websocket_spec.rb
@@ -9,6 +9,8 @@ describe 'zaqar::transport::websocket' do
           :bind  => '1',
           :port  => '2',
           :external_port  => '3',
+          :notification_bind  => '4',
+          :notification_port  => '5',
         }
       end
 
@@ -16,6 +18,8 @@ describe 'zaqar::transport::websocket' do
         is_expected.to contain_zaqar_config('drivers:transport:websocket/bind').with_value('1')
         is_expected.to contain_zaqar_config('drivers:transport:websocket/port').with_value('2')
         is_expected.to contain_zaqar_config('drivers:transport:websocket/external-port').with_value('3')
+        is_expected.to contain_zaqar_config('drivers:transport:websocket/notification-bind').with_value('4')
+        is_expected.to contain_zaqar_config('drivers:transport:websocket/notification-port').with_value('5')
       end
     end
   end

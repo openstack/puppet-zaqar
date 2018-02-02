462dd20b3720c4749c300e105566d375c3b4d54f
Mohammed Naser <mnaser@vexxhost.com>
2018-02-02 13:14:21 -0500

    Remove MongoDB integration and transform to using POI resources
    This patch removes MongoDB beaker tests as this is not a well
    tested use-case and sticks to the Swift/SQLalchemy configuration
    which is well tested.  It switches Beaker to use the same resources
    in integration.
    
    Depends-On: Ic1a4ea20ef65a832a986964fb61e80c4738740b5
    Change-Id: I9796a7bb2f25a2df53a1aeeb2814a91ca2c2cbec

--- spec/acceptance/zaqar_swift_spec.rb
+++ spec/acceptance/zaqar_swift_spec.rb
@@ -11,48 +11,8 @@ describe 'swift zaqar' do
       include ::openstack_integration::mysql
       include ::openstack_integration::keystone
       include ::openstack_integration::swift
-
-      class { '::memcached':
-        listen_ip => '127.0.0.1',
-      }
-
-      class { '::zaqar::keystone::auth':
-        password => 'a_big_secret',
-      }
-
-      class { '::zaqar::keystone::auth_websocket':
-        password => 'a_big_secret',
-      }
-
-      class {'::zaqar::management::sqlalchemy':
-        uri => 'mysql+pymysql://zaqar:a_big_secret@127.0.0.1/zaqar?charset=utf8',
-      }
-      class {'::zaqar::messaging::swift':
-        uri => 'swift://zaqar:a_big_secret:/service'
-      }
-      class {'::zaqar::keystone::authtoken':
-        password => 'a_big_secret',
-      }
-      class {'::zaqar::logging':
-        debug => true,
-      }
-      class {'::zaqar':
-        unreliable => true,
-      }
-      class {'::zaqar::server':
-        service_name => 'httpd',
-      }
-      include ::apache
-      class { '::zaqar::wsgi::apache':
-        ssl => false,
-      }
-      # run a second instance using websockets, the Debian system does
-      # not support the use of services to run a second instance.
-      if $::osfamily == 'RedHat' {
-        zaqar::server_instance{ '1':
-          transport => 'websocket'
-        }
-      }
+      include ::openstack_integration::memcached
+      include ::openstack_integration::zaqar
       EOS
 
 
@@ -68,4 +28,3 @@ describe 'swift zaqar' do
   end
 
 end
-

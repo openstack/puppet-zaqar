b6ed758a7805336a8c556ec19a269d7ca1edbbc0
Tobias Urdin <tobias.urdin@binero.se>
2019-02-08 12:31:52 +0100

    Use puppet 4 compatible mysql functions
    These was introduced in 6.0.0 and is required to
    support later version of puppetlabs-mysql.
    
    Change-Id: I4978e2706e983735f37f5441efbc6b1c8d8fd053

--- manifests/db/mysql.pp
+++ manifests/db/mysql.pp
@@ -49,7 +49,7 @@ class zaqar::db::mysql(
 
   ::openstacklib::db::mysql { 'zaqar':
     user          => $user,
-    password_hash => mysql_password($password),
+    password_hash => mysql::password($password),
     dbname        => $dbname,
     host          => $host,
     charset       => $charset,

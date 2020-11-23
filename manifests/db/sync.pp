6c5b691a85764cb44db6a9c5683afc430cf11b36
ZhongShengping <chdzsp@163.com>
2020-11-23 09:27:52 +0800

    Allow db sync timeouts to be configurable
    As Openstack projects continue to have longer database migration
    chains, the Puppet default timeout of 300 seconds for an execution
    is becoming too short a duration on some hardware, leading to timeouts.
    As projects continue to add more migration scripts without pruning
    the base, timeouts will continue to become more frequent unless
    this time can be expanded.
    
    Change-Id: I4a3941c5a21560c6246d22e89d4566dcdc95bfd6
    Closes-Bug: #1904962

--- manifests/db/sync.pp
+++ manifests/db/sync.pp
@@ -1,7 +1,15 @@
 #
 # Class to execute "zaqar-sql-db-manage upgrade head"
 #
-class zaqar::db::sync {
+# ==Parameters
+#
+# [*db_sync_timeout*]
+#   (Optional) Timeout for the execution of the db_sync
+#   Defaults to 300
+#
+class zaqar::db::sync(
+  $db_sync_timeout = 300,
+) {
 
   include zaqar::deps
 
@@ -12,6 +20,7 @@ class zaqar::db::sync {
     refreshonly => true,
     try_sleep   => 5,
     tries       => 10,
+    timeout     => $db_sync_timeout,
     logoutput   => on_failure,
     subscribe   => [
       Anchor['zaqar::install::end'],

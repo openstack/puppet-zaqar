2f89bc9979281d35f4b98aa5a637b00a52ff7a8d
Takashi Kajinami <tkajinam@redhat.com>
2022-03-08 09:27:37 +0900

    remove unicode literal from code
    All strings are considered as unicode string from Python 3.
    
    This patch drops the explicit unicode literal (u'...')
    appearances from the unicode strings.
    
    Change-Id: I31ffc4c99e12330cafd66810e0035388b5f33af6
    Co-Authored-By: LiZekun <2954674728@qq.com>

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -45,7 +45,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-copyright = u'2017, Puppet OpenStack Developers'
+copyright = '2017, Puppet OpenStack Developers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -185,8 +185,8 @@ htmlhelp_basename = 'puppet-zaqarReleaseNotesdoc'
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'puppet-zaqarReleaseNotes.tex', u'puppet-zaqar Release Notes Documentation',
-   u'2017, Puppet OpenStack Developers', 'manual'),
+  ('index', 'puppet-zaqarReleaseNotes.tex', 'puppet-zaqar Release Notes Documentation',
+   '2017, Puppet OpenStack Developers', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -215,8 +215,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'puppet-zaqarreleasenotes', u'puppet-zaqar Release Notes Documentation',
-     [u'2017, Puppet OpenStack Developers'], 1)
+    ('index', 'puppet-zaqarreleasenotes', 'puppet-zaqar Release Notes Documentation',
+     ['2017, Puppet OpenStack Developers'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -229,8 +229,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'puppet-zaqarReleaseNotes', u'puppet-zaqar Release Notes Documentation',
-   u'2017, Puppet OpenStack Developers', 'puppet-zaqarReleaseNotes', 'One line description of project.',
+  ('index', 'puppet-zaqarReleaseNotes', 'puppet-zaqar Release Notes Documentation',
+   '2017, Puppet OpenStack Developers', 'puppet-zaqarReleaseNotes', 'One line description of project.',
    'Miscellaneous'),
 ]
 

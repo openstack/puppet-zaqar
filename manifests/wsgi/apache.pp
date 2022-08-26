d34f26c31984ecdc07e6dc7e80dbfa5c1ef7c11f
Takashi Kajinami <tkajinam@redhat.com>
2022-08-26 16:53:21 +0900

    Add Apache WSGI logging parameters for pipe/syslog
    Add parameters for advanced logging configurations in Apache to
    support piped logging and support for syslog (via mod_syslog
    available in Apache >= 2.5.0)
    
    Co-Authored-By: Andy Botting <andy@andybotting.com>
    Change-Id: If07cac9bc41d173baeadbefb4dad3612c32ee369

--- manifests/wsgi/apache.pp
+++ manifests/wsgi/apache.pp
@@ -70,12 +70,28 @@
 #     The log file name for the virtualhost.
 #     Optional. Defaults to undef.
 #
+#   [*access_log_pipe*]
+#     Specifies a pipe where Apache sends access logs for the virtualhost.
+#     Optional. Defaults to undef.
+#
+#   [*access_log_syslog*]
+#     Sends the virtualhost access log messages to syslog.
+#     Optional. Defaults to undef.
+#
 #   [*access_log_format*]
 #     The log format for the virtualhost.
 #     Optional. Defaults to undef.
 #
 #   [*error_log_file*]
 #     The error log file name for the virtualhost.
+#     Optional. Defaults to undef.
+#
+#   [*error_log_pipe*]
+#     Specifies a pipe where Apache sends error logs for the virtualhost.
+#     Optional. Defaults to undef.
+#
+#   [*error_log_syslog*]
+#     Sends the virtualhost error log messages to syslog.
 #     Optional. Defaults to undef.
 #
 #   [*custom_wsgi_process_options*]
@@ -122,8 +138,12 @@ class zaqar::wsgi::apache (
   $threads                     = 1,
   $priority                    = 10,
   $access_log_file             = undef,
+  $access_log_pipe             = undef,
+  $access_log_syslog           = undef,
   $access_log_format           = undef,
   $error_log_file              = undef,
+  $error_log_pipe              = undef,
+  $error_log_syslog            = undef,
   $custom_wsgi_process_options = {},
   $headers                     = undef,
   $request_headers             = undef,
@@ -161,9 +181,13 @@ class zaqar::wsgi::apache (
     headers                     => $headers,
     request_headers             => $request_headers,
     vhost_custom_fragment       => 'WSGICallableObject app',
+    custom_wsgi_process_options => $custom_wsgi_process_options,
     access_log_file             => $access_log_file,
+    access_log_pipe             => $access_log_pipe,
+    access_log_syslog           => $access_log_syslog,
     access_log_format           => $access_log_format,
     error_log_file              => $error_log_file,
-    custom_wsgi_process_options => $custom_wsgi_process_options,
+    error_log_pipe              => $error_log_pipe,
+    error_log_syslog            => $error_log_syslog,
   }
 }

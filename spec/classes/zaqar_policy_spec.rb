d72accf703c2ebd68b9c3513982fdfd30366ad7d
Takashi Kajinami <tkajinam@redhat.com>
2023-01-23 14:43:31 +0900

    Expose policy_default_rule
    The option has been managed by the underlying puppet-oslo module but
    has not been configurable. This introduces the parameter to customize
    the option.
    
    Change-Id: Iac1ebf8af8900e9e351ef359f1c5c2e4c1704d00

--- spec/classes/zaqar_policy_spec.rb
+++ spec/classes/zaqar_policy_spec.rb
@@ -9,6 +9,7 @@ describe 'zaqar::policy' do
           :enforce_scope        => false,
           :enforce_new_defaults => false,
           :policy_path          => '/etc/zaqar/policy.yaml',
+          :policy_default_rule  => 'default',
           :policy_dirs          => '/etc/zaqar/policy.d',
           :policies             => {
             'context_is_admin' => {
@@ -37,6 +38,7 @@ describe 'zaqar::policy' do
           :enforce_scope        => false,
           :enforce_new_defaults => false,
           :policy_file          => '/etc/zaqar/policy.yaml',
+          :policy_default_rule  => 'default',
           :policy_dirs          => '/etc/zaqar/policy.d',
         )
       end
